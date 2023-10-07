f205db09ead12fa9772a53221d93042db02caebf
12ryn <zryn1212@gmail.com>
2023-10-07 11:30:53 +0800

    hh

--- main/src/TextBasedGame.java
+++ main/src/TextBasedGame.java
@@ -1,31 +1,37 @@
+package src;
+
+import java.util.Scanner;
+
 public class TextBasedGame {
 
     public static void main(String[] args) {
 
+
         System.out.println("Instructions: When promopted for a response, please either type in your word or a number corresponding to the given choices.");
 
-        Scanner scanner = new Scanner.(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your desired name: ");
         String name = scanner.nextLine();
-        scanner.nextLine()
 
         System.out.println("Guma: " + name + ", I have been waiting for you. I've been looking for the Stone of Kaloxi for as long as I could remember.");
         System.out.println("(1) No \n(2) Of course, it is my pleasure.");
         int response = scanner.nextInt();
 
-        if(response == 1) {
-            System.out.println("Guma: You know what, just leave.");
-            gameCondition(false);
-        }
+        if(response >= 1) {
+            if (response == 1) {
+                System.out.println("Guma: You know what, just leave.");
+                System.out.println(gameCondition(false));
+            }
 
-        else if(response == 2){
-            System.out.println("Guma: Knew I could count on you. Let's get to work.")
-            gameCondition(true);
-        }
+            else if (response == 2) {
+                System.out.println("Guma: Knew I could count on you. Let's get to work.");
+                gameCondition(true);
+            }
 
-        else{
-            System.out.println("Guma: You must be funny. Let's go.");
-            gameCondition(true);
+            else {
+                System.out.println("Guma: You must be funny. Let's go.");
+                gameCondition(true);
+            }
         }
 
         System.out.println("You & Guma arrive to the land of Melser");
@@ -42,21 +48,6 @@ public class TextBasedGame {
         System.out.println("You & Guma arrive at the mountain. Choose an action:");
         System.out.println("(1) Proceed and enter mount Melser. \n(2) Wait and observe conditions. \n(3) Leave and go home.");
 
-        if(response == 1) {
-            System.out.println("Guma: You know what, just leave.");
-            gameCondition(false);
-        }
-
-        else if(response == 2){
-            System.out.println("Guma: Knew I could count on you. Let's get to work.")
-            gameCondition(true);
-        }
-
-        else{
-            System.out.println("Guma: You must be funny. Let's go.");
-            gameCondition(true);
-        }
-
     }
 
     public static String gameCondition(boolean winLoss){
@@ -65,11 +56,8 @@ public class TextBasedGame {
             return "You have failed to retrieve the Stone of Kaloxi. Game over.";
         }
         else{
-            return;
+            return "";
         }
     }
-
-    public static
-
 
 }
